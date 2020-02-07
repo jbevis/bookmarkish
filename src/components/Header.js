949e8126a7279b7168209327160a7f48a4ff0074
John Bevis <john.bevis@nrel.gov>
2020-02-07 11:57:12 -0700

    adding subscriptions and pagination to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,25 +8,33 @@ class Header extends Component {
     const authToken = localStorage.getItem(AUTH_TOKEN);
 
     return (
-      <div className="flex pa1 justify-between nowrap orange">
-        <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
+      <div className='flex pa1 justify-between nowrap orange'>
+        <div className='flex flex-fixed black'>
+          <div className='fw7 mr1'>Hacker News</div>
+          <Link to='/' className='ml1 no-underline black'>
             new
           </Link>
+          <div className='ml1'>|</div>
+          <Link to="/top" className="ml1 no-underline black">
+            top
+          </Link>
+          <div className="ml1">|</div>  
+          <Link to='/search' className='ml1 no-underline black'>
+            search
+          </Link>
           {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
+            <div className='flex'>
+              <div className='ml1'>|</div>
+              <Link to='/create' className='ml1 no-underline black'>
                 submit
             </Link>
             </div>
           )}
         </div>
-        <div className="flex flex-fixed">
+        <div className='flex flex-fixed'>
           {authToken ? (
             <div
-              className="ml1 pointer black"
+              className='ml1 pointer black'
               onClick={() => {
                 localStorage.removeItem(AUTH_TOKEN)
                 this.props.history.push(`/`)
@@ -35,7 +43,7 @@ class Header extends Component {
               logout
           </div>
           ) : (
-              <Link to="/login" className="ml1 no-underline black">
+              <Link to='/login' className='ml1 no-underline black'>
                 login
           </Link>
             )}
